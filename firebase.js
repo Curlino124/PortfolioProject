c9906902cc9409c79620887ffc8ee4aff9d59ab8
Curlino124 <stephan.scholz124@gmail.com>
2024-10-21 16:06:38 +0200

    Erster Commi

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCMucQlOsFMKYSfb3u2-9GuQfIvUjf2KCw',
+  authDomain: 'portfolioproject-f5234.firebaseapp.com',
+  projectId: 'portfolioproject-f5234',
+  storageBucket: 'portfolioproject-f5234.appspot.com',
+  messagingSenderId: '761568847974',
+  appId: '1:761568847974:web:5151b512f448a6047ee5a7',
+}
+
+const app = initializeApp(firebaseConfig)
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center items-center h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">Das ist ein Test !!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style></style>
